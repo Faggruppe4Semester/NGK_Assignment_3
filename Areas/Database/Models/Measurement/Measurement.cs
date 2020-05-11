d6524b99a471b594edc00e0d7fa6310a558d9ae2
Worly97 <frede0@hotmail.com>
2020-05-11 13:38:46 +0200

    Place added to own file

--- Areas/Database/Models/Measurement/Measurement.cs
+++ Areas/Database/Models/Measurement/Measurement.cs
@@ -21,12 +21,4 @@ namespace NGK_Assignment_3.Areas.Database.Models
 
     }
 
-    public class Place
-    {
-        public string Name { get; set; }
-        public double Lat { get; set; }
-        public double Lon { get; set; }
-
-        public List<Measurement> Measurements { get; set; }
-    }
 }
---
+++ Areas/Database/Models/Measurement/Place.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace NGK_Assignment_3.Areas.Database.Models
+{
+    public class Place
+    {
+        public string Name { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+
+        public List<Measurement> Measurements { get; set; }
+    }
+}
